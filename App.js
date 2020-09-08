e88b57b57154aab60376283fa97165ce72e883b5
Ununpentium <ununpentium@outlook.com.br>
2020-09-08 11:48:14 -0300

    MASTER

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Principal from './src/views/Principal';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return(
+    <Principal/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/views/Principal/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './style';
+
+export default function Princiapl() {
+    return(
+        <View style={styles.container}>
+            <Text>PLGTAR-MOBILE</Text>
+        </View>
+    )
+}
